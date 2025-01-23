33d4dea3ac8d9c22ecddbaf2b4c37a938b5c24b1
halid <you@example.com>
2025-01-23 16:34:16 +0800

    rev

--- belajar/js/test1.js
+++ belajar/js/test1.js
@@ -1,5 +1,17 @@
 // toggle
 const navbarNav = document.querySelector('.nav-link');
+const Menu = document.querySelector('#menu');
+
+
+// kondisi ketika click di logo menu
 document.querySelector('#menu').onclick=()=>{
 navbarNav.classList.toggle('active');
 };
+
+// kondisis ketika click di luar logo menu
+document.addEventListener('click', function(e){
+if (!Menu.contains(e.target)&&(!navbarNav.contains(e.target))){ {
+    navbarNav.classList.remove('active'); 
+}
+}
+})
