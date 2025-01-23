4defae3c502c98a6570eb83dc865c2440fcb1037
halid <you@example.com>
2025-01-23 16:11:50 +0800

    perbaikan

--- belajar/js/test1.js
+++ belajar/js/test1.js
@@ -1,2 +1,5 @@
 // toggle
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector('.nav-link');
+document.querySelector('#menu').onclick=()=>{
+navbarNav.classList.toggle('active');
+};
